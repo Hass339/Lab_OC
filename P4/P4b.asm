4c81784900698f1a4f51673c2d78fb33f47e0b0b
Hass339 <hassiel.flores@uabc.edu.mx>
2025-03-28 11:47:51 -0700

    Update P4b.asm

--- P4/P4b.asm
+++ P4/P4b.asm
@@ -1,18 +1,18 @@
-%include "../LIB/pc_io.inc"   ; incluir declaraciones de procedimiento externos
-                               ; que se encuentran en la biblioteca libpc_io.a
+%include "../LIB/pc_io.inc"   
+                               
 
 section .text
-    global _start       ; referencia para inicio de programa
+    global _start       
     
 _start:
-    lea esi, [msg]      ; Cargar la dirección de msg en esi
-    mov byte [esi+23], 'X'  ; Reemplazar 'x' por 'X' (posición 23)
+    lea esi, [msg]      
+    mov byte [esi+23], 'X'  
 
-    mov edx, msg        ; edx = dirección de la cadena msg
-    call puts           ; imprime cadena msg terminada en valor nulo (0)
+    mov edx, msg       
+    call puts           
 
-    mov eax, 1          ; seleccionar llamada al sistema para fin de programa
-    int 0x80            ; llamada al sistema - fin de programa
+    mov eax, 1          
+    int 0x80            
 
 section .data
 msg db  'abcdefghijklmnopqrstuvwxyz0123456789', 0xa, 0  ; Cadena original
