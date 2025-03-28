5fed83252007d822b042e6a016160bb7f9c58419
Hass339 <hassiel.flores@uabc.edu.mx>
2025-03-28 11:49:37 -0700

    Update P4a.asm

--- P4/P4a.asm
+++ P4/P4a.asm
@@ -1,15 +1,14 @@
-%include "../LIB/pc_io.inc"  	; incluir declaraciones de procedimiento externos
-								; que se encuentran en la biblioteca libpc_io.a
-
+%include "../LIB/pc_io.inc"  	
+								
 section	.text
-	global _start       ;referencia para inicio de programa
+	global _start    
 	
 _start:                   
-	mov edx, msg		; edx = dirección de la cadena msg
-	call puts			; imprime cadena msg terminada en valor nulo (0)
+	mov edx, msg		
+	call puts			
 
-	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
-	int	0x80        	; llamada al sistema - fin de programa
+	mov	eax, 1	    	
+	int	0x80        	
 
 section	.data
 msg	db  'Zbcdefghijklmnopqrstuvwxyz0123456789',0xa,0 
